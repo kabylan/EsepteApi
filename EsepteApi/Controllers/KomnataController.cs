80ee72abd2be13ffdc3a0a0d5acff9f9072df877
Kushtar <eseptehelp@gmail.com>
2020-09-20 01:00:30 +0600

    Alocal and EsepteLinkVersion

--- EsepteApi/Controllers/KomnataController.cs
+++ EsepteApi/Controllers/KomnataController.cs
@@ -8,6 +8,7 @@ using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Net;
 
 namespace EsepteApi.Controllers
 {
@@ -17,6 +18,7 @@ namespace EsepteApi.Controllers
     {
         IWebHostEnvironment _appEnvironment;
         private static readonly HttpClient client = new HttpClient();
+        private static readonly string esepteKomnataUrl = "http://localhost:6012";
 
         public KomnataController(IWebHostEnvironment appEnvironment)
         {
@@ -25,50 +27,26 @@ namespace EsepteApi.Controllers
 
         // для распознования типа помещения на фотографии
         [HttpGet]
-        public async Task<string> Get(string imageName)
+        public async Task<string> Get(string imageLink)
         {
-            // получить распознование
-            var response = await client.GetAsync("http://localhost:6012/komnata/" + imageName);
-            var responseString = await response.Content.ReadAsStringAsync();
 
+            // путь к скачиванию файла
+            string imageName = "Image_" + DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss") + Path.GetExtension(imageLink);
+            string path = "/Uploads/" + imageName;
+            Debug.Print(path);
+
+            // скачать картинку по ссылке
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(new Uri(imageLink), _appEnvironment.WebRootPath + path);
+            }
+
+            // получить распознование
+            var response = await client.GetAsync(esepteKomnataUrl + "/komnata/" + imageName);
+            var responseString = await response.Content.ReadAsStringAsync();
 
             return responseString;
         }
-
-        //// для распознования типа помещения на фотографии
-        //[HttpPost]
-        //public async Task<IActionResult> Recognize(IFormFileCollection uploads)
-        //{
-        //    // сохранить файлы
-        //    List<Komnata> komnatas = await SaveFiles(uploads);
-
-        //    // получить распознование
-
-
-        //    return RedirectToAction("Index");
-        //}
-
-        //// сохранения файла
-        //private async Task<List<Komnata>> SaveFiles(IFormFileCollection uploads)
-        //{
-        //    List<Komnata> komnatas = new List<Komnata>();
-
-        //    foreach (var uploadedFile in uploads)
-        //    {
-        //        // путь к папке Files
-        //        string path = @"C:\Users\esept\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs\home\apollo\EsepteKomnataUploads\" + uploadedFile.FileName;
-
-        //        // сохраняем файл в папку Files в каталоге wwwroot
-        //        using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
-        //        {
-        //            await uploadedFile.CopyToAsync(fileStream);
-        //        }
-        //        komnatas.Add(new Komnata { Name = uploadedFile.FileName, Path = path });
-        //    }
-
-        //    return komnatas;
-        //}
-
 
     }
 }
