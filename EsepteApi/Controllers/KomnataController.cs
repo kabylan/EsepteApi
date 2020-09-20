5e21c6a1503317c92e05964b6f926389c689e747
Kushtar <eseptehelp@gmail.com>
2020-09-20 23:55:32 +0600

    ML.NET Used

--- EsepteApi/Controllers/KomnataController.cs
+++ EsepteApi/Controllers/KomnataController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Net;
+using EsepteApi.MachineLearning.Model;
 
 namespace EsepteApi.Controllers
 {
@@ -25,27 +26,98 @@ namespace EsepteApi.Controllers
             _appEnvironment = appEnvironment;
         }
 
-        // для распознования типа помещения на фотографии
+        //// для распознования типа помещения на фотографии
+        //[HttpGet]
+        //public async Task<string> Get(string imageLink)
+        //{
+
+        //    // путь к скачиванию файла
+        //    string imageName = "Image_" + DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss") + Path.GetExtension(imageLink);
+        //    string path = "/Uploads/" + imageName;
+        //    Debug.Print(path);
+
+        //    // скачать картинку по ссылке
+        //    using (WebClient client = new WebClient())
+        //    {
+        //        client.DownloadFile(new Uri(imageLink), _appEnvironment.WebRootPath + path);
+        //    }
+
+        //    // получить распознование
+        //    var response = await client.GetAsync(esepteKomnataUrl + "/komnata/" + imageName);
+        //    var responseString = await response.Content.ReadAsStringAsync();
+
+        //    return responseString;
+        //}
+
         [HttpGet]
-        public async Task<string> Get(string imageLink)
+        public string Get(string imageLink)
         {
 
             // путь к скачиванию файла
             string imageName = "Image_" + DateTime.Now.ToString("MM_dd_yyyy_HH_mm_ss") + Path.GetExtension(imageLink);
-            string path = "/Uploads/" + imageName;
+            string path = _appEnvironment.WebRootPath + "/Uploads/" + imageName;
             Debug.Print(path);
-
             // скачать картинку по ссылке
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(new Uri(imageLink), _appEnvironment.WebRootPath + path);
+                client.DownloadFile(new Uri(imageLink), path);
+            }
+            // Получить распознование
+
+            // Create single instance of sample data from first line of dataset for model input
+            ModelInput sampleData = new ModelInput()
+            {
+                ImageSource = path,
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = ConsumeModel.Predict(sampleData);
+
+            System.Diagnostics.Debug.Print("Using model to make single prediction -- Comparing actual Label with predicted Label from sample data...\n\n");
+            System.Diagnostics.Debug.Print($"ImageSource: {sampleData.ImageSource}");
+            System.Diagnostics.Debug.Print($"\n\nPredicted Label value {predictionResult.Prediction} \nPredicted Label scores: [{String.Join(",", predictionResult.Score)}]\n\n");
+            System.Diagnostics.Debug.Print("=============== End of process, hit any key to finish ===============");
+
+
+            string komnataTypeRU = "";
+            switch(predictionResult.Prediction)
+            {
+                case "bathroom":
+                    komnataTypeRU = "ванная";
+                    break;
+                case "bedroom":
+                    komnataTypeRU = "спальняя";
+                    break;
+                case "corridor":
+                    komnataTypeRU = "коридор";
+                    break;
+                case "dining_room":
+                    komnataTypeRU = "столовая";
+                    break;
+                case "elevator":
+                    komnataTypeRU = "лифт";
+                    break;
+                case "garage":
+                    komnataTypeRU = "гараж";
+                    break;
+                case "kitchen":
+                    komnataTypeRU = "кухня";
+                    break;
+                case "livingroom":
+                    komnataTypeRU = "гостиная";
+                    break;
+                case "office":
+                    komnataTypeRU = "офис";
+                    break;
+                case "waitingroom":
+                    komnataTypeRU = "приемная";
+                    break;
+                default:
+                    komnataTypeRU = "-";
+                    break;
             }
 
-            // получить распознование
-            var response = await client.GetAsync(esepteKomnataUrl + "/komnata/" + imageName);
-            var responseString = await response.Content.ReadAsStringAsync();
-
-            return responseString;
+            return "{\"komnataType\": \"" + komnataTypeRU + "\" }";
         }
 
     }
--- EsepteApi/Controllers/WeatherForecastController.cs
+++ EsepteApi/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using EsepteApi.MachineLearning.Model;
 
 namespace EsepteApi.Controllers
 {
@@ -24,16 +25,36 @@ namespace EsepteApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            // Create single instance of sample data from first line of dataset for model input
+            ModelInput sampleData = new ModelInput()
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                ImageSource = @"C:\Users\esept\Downloads\archive(5)\indoorCVPR_09\ImagesMini\bathroom\008.jpg",
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = ConsumeModel.Predict(sampleData);
+
+            System.Diagnostics.Debug.Print("Using model to make single prediction -- Comparing actual Label with predicted Label from sample data...\n\n");
+            System.Diagnostics.Debug.Print($"ImageSource: {sampleData.ImageSource}");
+            System.Diagnostics.Debug.Print($"\n\nPredicted Label value {predictionResult.Prediction} \nPredicted Label scores: [{String.Join(",", predictionResult.Score)}]\n\n");
+            System.Diagnostics.Debug.Print("=============== End of process, hit any key to finish ===============");
+
+            return predictionResult.Prediction;
         }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
     }
 }
